231bb34d5384200aff67c89fa3754663c0adb3af
ahmetcng33 <ahmetcingi333@gmail.com>
2023-05-13 12:20:07 +0300

    s

---
+++ js/js5-functions/fonctions.js
@@ -0,0 +1,26 @@
+// ? =====================================
+// ?           FUNCTIONS
+// ? ======================================
+
+console.log("** FUNC DECLARATION **")
+
+//* ORNEK1:
+//*****************************************/
+
+//! Declaration (Fonksiyonun tanimlanmasi)
+function yazdirMerhaba() {
+  // alert("Merhaba FS14")
+  console.log("Merhaba FS14")
+}
+
+yazdirMerhaba() //! invoke, call
+yazdirMerhaba() //! invoke, call
+yazdirMerhaba() //! invoke, call
+
+//* ORNEK2:
+//*****************************************/
+function yazdirParametre(name) {
+  console.log("Merhaba", name)
+}
+yazdirParametre("FS15")
+yazdirParametre("FS16")
