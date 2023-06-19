060e1b659a701a8425646c3d8311c16a49df6c80
ahmetcng33 <ahmetcingi333@gmail.com>
2023-06-19 16:17:18 +0300

    selectors.js

---
+++ js/class-notes/js12-dom/travesingDOM.js
@@ -0,0 +1,16 @@
+console.log("**** TRAVERSING DOM ****")
+
+//!=========================================================
+//!               TRAVERSİNG DOM
+//!=========================================================
+//? Bir HTML elementi üzerinden başka elementleri secmeye
+//?DOM traversing denilir. Özellikle dinamik olarak değişen 
+//? (silme veya yeni ekleme gibi) HTML elemanlarinin seecerken 
+//? gerekli olabilir.
+
+//? Secme islemi 3 yonlu yapilabilir.
+//? - Yukari yönde (Parent)
+//? - Asagi yönde (Child)
+//? - Yatay yönde (Sibling)
+
+//! 1- Yukari Yonde Traverse
