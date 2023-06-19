ccfb586f378dc0af179be311eef3a663669168e8
ahmetcng33 <ahmetcingi333@gmail.com>
2023-06-19 16:50:10 +0300

    js12-dom

--- js/class-notes/js12-dom/travesingDOM.js
+++ js/class-notes/js12-dom/travesingDOM.js
@@ -14,3 +14,53 @@ console.log("**** TRAVERSING DOM ****")
 //? - Yatay yönde (Sibling)
 
 //! 1- Yukari Yonde Traverse
+//!-------------------------------------------------------------
+
+const addItem = document.querySelector(".add-item")
+console.log(addItem)
+
+console.log(addItem.parentElement) //* Main
+console.log(addItem.parentElement.parentElement) //* Body
+
+//* closest(): belirtilen elementin parent'larin ilk uygun parent'i secer
+
+const list = document.querySelector(".list")
+console.log(list.textContent)
+
+console.log(list.closest("section"))
+
+console.log(list.closest("body").querySelector("header h1").innerText)
+
+//! 2 - Asagi Yönde Traverse
+//!-------------------------------------------------------------
+//? Children, firstElementChild(), lastElementChild
+
+console.log(addItem.children) //HTMLCollection(3) [h2, input#input, input#btn, input: input#input, btn: input#btn]
+
+console.log(addItem.children[2])
+
+console.log(addItem.firstElementChild) //*h2
+console.log(addItem.lastElementChild) //* ADD Buton
+
+console.log(addItem.firstChild) //* merhaba section
+
+//* firstChild bir yazi elementinin icerisindeki yazi dugumunu verir.
+console.log(document.querySelector("h1").firstChild) //*DOM SELECTORS
+
+//! 3- Yatay Yonde Traverse
+//!-------------------------------------------------------------
+//* nextElementSbigling ,previouselementSibling
+
+const ul = document.querySelector("ul")
+
+const lists = ul.children
+console.log(lists)
+
+const js = lists[2]
+console.log(js.innerHTML) //*JavaScript
+
+const css = js.previousElementSibling
+console.log(css.innerHTML) //*CSS
+
+const django = js.nextElementSibling.nextElementSibling
+console.log(django.innerHTML) //Django
